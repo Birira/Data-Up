059261ecfbc167c54287faedee055a1765a42a74
birira <vicente.pereira.ch@gmail.com>
2025-05-22 12:57:25 -0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Navi } from './Components/Navi';
+
+function App() {
+
+  return (
+    <div>
+      <Navi />
+    </div>
+  )
+};
+
+export default App;
---
+++ frontend/src/Components/Navi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navi = () => {
+    return (
+        <div className='bg-dark'>
+            <h1 >Navbar</h1>
+        </div>
+    )
+};
